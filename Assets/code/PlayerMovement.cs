446861c2c6153d94749a48568972e09f1fb969a4
IllegalTempo <jedtsui0704@gmail.com>
2026-03-15 22:36:49 +0000

    added light

--- Assets/code/PlayerMovement.cs
+++ Assets/code/PlayerMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class PlayerMovement : Selectable, IFreezable, IResetable
 {
